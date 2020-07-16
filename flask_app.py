9adca93d802384b486f790ec2a9c33787e4e8c58
Rohan Vedula <61258607+rohanvedula@users.noreply.github.com>
2020-07-16 16:01:59 -0400

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -4,52 +4,7 @@ from flask import request
 from flask import after_this_request
 from flask_cors import CORS
 from flask_cors import cross_origin
-import nltk
-from nltk.corpus import stopwords
-nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-from nltk.tokenize import word_tokenize, sent_tokenize
-
-def get_message(text):
-    stopWords = set(stopwords.words("english"))
-    words = word_tokenize(text)
-
-    freqTable = dict()
-    for word in words:
-        word = word.lower()
-        if word in stopWords:
-            continue
-        if word in freqTable:
-            freqTable[word] += 1
-        else:
-            freqTable[word] = 1
-
-    sentences = sent_tokenize(text)
-    sentenceValue = dict()
-
-    for sentence in sentences:
-        for word, freq in freqTable.items():
-            if word in sentence.lower():
-                if sentence in sentenceValue:
-                    sentenceValue[sentence] += freq
-                else:
-                    sentenceValue[sentence] = freq
-
-
-
-    sumValues = 0
-    for sentence in sentenceValue:
-        sumValues += sentenceValue[sentence]
-
-    average = int(sumValues / len(sentenceValue))
-
-    summary = ''
-    for sentence in sentences:
-        if (sentence in sentenceValue) and (sentenceValue[sentence] > (1.2 * average)):
-            summary += " " + sentence
-
-    return summary
+from get_compressed import get_message
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -58,11 +13,6 @@ app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
 
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
-#@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
-
-#@app.route('/')
 @app.route('/', methods = ['GET', 'POST'])
 @cross_origin(origin='/*',headers=['Content- Type','Authorization'])
 def get_response():
